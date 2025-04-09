908a312420946905ca428a3981b4cf32ef564839
Dariellambert <36234875+Dariellambert@users.noreply.github.com>
2025-04-09 17:40:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ if st.button('Send'):
     if user_input:
         # Send the user input to the Flask backend
         response = requests.post(
-            "https://chat-botfinalproject-production.up.railway.app/chat",  # Adjust this URL to match where your Flask server is running
+            "https://chat-botfinalproject-new-production.up.railway.app/chat",  # Adjust this URL to match where your Flask server is running
             headers={"Content-Type": "application/json"},
             data=json.dumps({"message": user_input})
         )
